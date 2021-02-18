140f0234ebcee3a7aa120694aac30a8647ba7ad9
Vysakh Sreenivasan <vysakh@pipesort.com>
2021-02-18 21:45:47 +0530

    Add eslint unicorn config
    Youtube video: https://www.youtube.com/watch?v=5hnyoyyyo8A

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,50 @@
 module.exports = {
   plugins: ["prettier"],
-  extends: ["airbnb-typescript", "airbnb/hooks", "prettier", "prettier/react"],
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "prettier",
+    "prettier/react",
+    "plugin:unicorn/recommended",
+  ],
   rules: {
     "prettier/prettier": "error",
+    "unicorn/no-array-for-each": "off",
+    "unicorn/no-array-reduce": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/no-null": "off",
+    "unicorn/filename-case": [
+      "error",
+      {
+        case: "camelCase",
+      },
+    ],
   },
   parserOptions: {
     project: "./tsconfig.json",
   },
+  overrides: [
+    {
+      files: ["src/components/**/*.tsx"],
+      rules: {
+        "unicorn/filename-case": [
+          "error",
+          {
+            case: "pascalCase",
+          },
+        ],
+      },
+    },
+    {
+      files: ["src/pages/**/*.tsx"],
+      rules: {
+        "unicorn/filename-case": [
+          "error",
+          {
+            case: "kebabCase",
+          },
+        ],
+      },
+    },
+  ],
 };
