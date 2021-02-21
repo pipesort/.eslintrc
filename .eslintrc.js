fb08460a267da47b96ab8cb1ba99574ea0335b94
Vysakh Sreenivasan <vysakh@pipesort.com>
2021-02-21 23:27:19 +0530

    Add custom eslint-import related config.
    Also add prettier for this project.
    
    Youtube video for the eslint-import: https://youtu.be/d6Xa87sObGo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,85 @@
 module.exports = {
-  plugins: ["prettier"],
+  plugins: ['prettier'],
   extends: [
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "prettier",
-    "prettier/react",
-    "plugin:unicorn/recommended",
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
+    'prettier/react',
+    'plugin:unicorn/recommended',
   ],
   rules: {
-    "prettier/prettier": "error",
-    "unicorn/no-array-for-each": "off",
-    "unicorn/no-array-reduce": "off",
-    "unicorn/prevent-abbreviations": "off",
-    "unicorn/no-null": "off",
-    "unicorn/filename-case": [
-      "error",
+    'import/no-namespace': 'error',
+    'import/no-relative-parent-imports': 'error',
+    'import/no-anonymous-default-export': [
+      'error',
       {
-        case: "camelCase",
+        allowArray: true,
+        allowArrowFunction: false,
+        allowAnonymousClass: false,
+        allowAnonymousFunction: false,
+        allowCallExpression: true, // The true value here is for backward compatibility
+        allowLiteral: true,
+        allowObject: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: 'consts/**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: 'components/**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: 'helpers/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    'prettier/prettier': 'error',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'camelCase',
+        ignore: ['App.tsx'],
       },
     ],
   },
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
   },
   overrides: [
     {
-      files: ["src/components/**/*.tsx"],
+      files: ['src/components/**/*.tsx'],
       rules: {
-        "unicorn/filename-case": [
-          "error",
+        'unicorn/filename-case': [
+          'error',
           {
-            case: "pascalCase",
+            case: 'pascalCase',
           },
         ],
       },
     },
     {
-      files: ["src/pages/**/*.tsx"],
+      files: ['src/pages/**/*.tsx'],
       rules: {
-        "unicorn/filename-case": [
-          "error",
+        'unicorn/filename-case': [
+          'error',
           {
-            case: "kebabCase",
+            case: 'kebabCase',
           },
         ],
       },
